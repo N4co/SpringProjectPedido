4b8713002ea26869841fb80f3d9eaaa78f1e642d
n4co <nf.oliveira@hotmail.com>
2021-11-09 15:59:29 -0300

    Criando repository e service

---
+++ src/main/java/com/nelioalves/cursomc/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	
+	
+	}
+}
