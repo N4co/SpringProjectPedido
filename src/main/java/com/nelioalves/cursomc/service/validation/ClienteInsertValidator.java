fbf7bb461d55c1e7eab5c0e7516cec9bc06ec4b7
n4co <nf.oliveira@hotmail.com>
2021-11-17 23:17:37 -0300

    Validação Customizada

--- src/main/java/com/nelioalves/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/nelioalves/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,14 +6,24 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.enums.TipoCliente;
 import com.nelioalves.cursomc.dto.ClienteNewDTO;
+import com.nelioalves.cursomc.repository.ClienteRepository;
 import com.nelioalves.cursomc.resources.exception.FieldMessage;
 import com.nelioalves.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator <ClienteInsert, ClienteNewDTO> {
 	private ClienteNewDTO objDTO;
 
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +40,13 @@ public class ClienteInsertValidator implements ConstraintValidator <ClienteInser
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido "));
 		
 		}
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já Existente"));
+		}
+		
+		
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
